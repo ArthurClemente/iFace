ef39e31cf57c66c79b6854c412e083bef9f4957c
ArthurClemente <arthurclementte@hotmail.com>
2021-11-01 16:36:04 -0300

    Classe RegistrosLogin referenciada e instanciada no construtor de Conta. Implementadas as funções que retornam os arrays com os emails e nomes de usuários cadastrados.

--- src/Modelo/Conta/Conta.php
+++ src/Modelo/Conta/Conta.php
@@ -2,18 +2,24 @@
 
 namespace Projeto\Iface\Modelo\Conta; // Projeto\Iface é o "src" do namespace
 
+require_once 'src/Modelo/Autoloader/autoload.php';
+
+use Projeto\Iface\Modelo\Service\RegistrosLogin;
+
 class Conta
 {
-// -----------------Atributos e construtor-----------------
+    // -----------------Atributos e construtor-----------------
     private $login;
     private $senha;
     private $nomeUsuario;
+    private $registro;
 
     public function __construct(string $login, string $senha, string $nomeUsuario)
     {
         $this->login = $login;
         $this->senha = $senha;
         $this->nomeUsuario = $nomeUsuario;
+        $this->registro = new RegistrosLogin($login, $nomeUsuario);
     }
 // --------------------------------------------------------
 
@@ -39,8 +45,20 @@ class Conta
     }
 // --------------------------------------------------------
 
-public function podeAutenticar(string $login, string $senha): bool
-{
-    return $login === $this->login && $senha === $this->senha;
-}
+// --------------------Funcionalidades---------------------
+    public function podeAutenticar(string $login, string $senha): bool // Função que passa para o teste de login da classe Perfil se o email e a senha informados estão corretos.
+    {
+        return $login === $this->login && $senha === $this->senha;
+    }
+
+    public function verificarRegistroEmails(): array // Retorna o array contendo todos os emails registrados de cada instância de Conta.
+    {
+        return $this->registro->getRegistroEmails();   
+    }
+
+    public function verificarRegistroNomes(): array // Retorna o array contendo todos os nomes de usuário registrados de cada instância de Conta.
+    {
+        return $this->registro->getRegistroNomes();
+    }
+// --------------------------------------------------------
 }
