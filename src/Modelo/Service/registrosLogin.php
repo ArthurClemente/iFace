cf123ac92b8207e2054f4013b82d7b0991183e9d
ArthurClemente <arthurclementte@hotmail.com>
2021-11-06 16:53:52 -0300

    Agora a senha é salva no array junto do email e nome de usuário.

--- src/Modelo/Service/registrosLogin.php
+++ src/Modelo/Service/registrosLogin.php
@@ -7,16 +7,16 @@ class RegistrosLogin
     private static $registroContas = array();
     private static $numeroDeContas;
 
-    public function __construct(string $email, string $nomeUsuario)
+    public function __construct(string $email, string $senha, string $nomeUsuario)
     {
         self::$numeroDeContas += 1;
-        $this->incrementaArray($email, $nomeUsuario);
+        $this->incrementaArray($email, $senha, $nomeUsuario);
     }
 
-    public function incrementaArray(string $email, string $nomeUsuario): void
+    public function incrementaArray(string $email, string $senha, string $nomeUsuario): void
     {
-        $listaContas = array($email, $nomeUsuario);
-        array_push(self::$registroContas, $listaContas);
+        $dadosConta = array($email, $senha, $nomeUsuario);
+        array_push(self::$registroContas, $dadosConta);
     }
 
     public function getRegistroContas(): array
