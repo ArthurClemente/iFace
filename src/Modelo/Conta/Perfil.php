52188be410133c2984d5b77c48b0e5aac5063606
ArthurClemente <arthurclementte@hotmail.com>
2021-11-06 16:52:46 -0300

    Função tentaLogin() realocada para a classe Menu.

--- src/Modelo/Conta/Perfil.php
+++ src/Modelo/Conta/Perfil.php
@@ -8,15 +8,6 @@ use Projeto\Iface\Modelo\Conta\Conta;
 
 class Perfil
 {
-    // Função que verifica se o login e a senha são iguais aos parâmetros utilizados no construtor da classe Conta.
-    public function tentaLogin(Conta $conta, string $login, string $senha): void
-    {
-        if ($conta->podeAutenticar($login, $senha)) {
-            echo "Login efetuado com sucesso!" . PHP_EOL;
-        } else {
-            echo "Login ou senha incorretos. Tente novamente" . PHP_EOL;
-        }
-    }
 
     //-------------------Funções de criação/alteração de atributos do perfil-----------------------
     public function alteraNomeUsuario(Conta $conta, string $novoNome): void
