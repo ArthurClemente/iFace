04818d461e02193ca5ca8542d14ad39c617a6e0d
ArthurClemente <arthurclementte@hotmail.com>
2021-11-07 16:13:38 -0300

    Criados os atributos email, senha, nomeUsuario, endereco e telefone. Função alteraNomeUsuario modificada para alteraDados. A opção de alterar nome de usuário foi implementada e a opção de adicionar endereço falta salvar os dados no array de registros.

--- src/Modelo/Conta/Perfil.php
+++ src/Modelo/Conta/Perfil.php
@@ -8,15 +8,64 @@ use Projeto\Iface\Modelo\Conta\Conta;
 
 class Perfil
 {
+    private $email;
+    private $senha;
+    private $nomeUsuario;
+    public $endereco = array();
+    public $telefone;
+
+    public function __construct(Conta $conta)
+    {
+        $this->email = $conta->getEmail();
+        $this->senha = $conta->getSenha();
+        $this->nomeUsuario = $conta->getNomeUsuario();
+    }
 
     //-------------------Funções de criação/alteração de atributos do perfil-----------------------
-    public function alteraNomeUsuario(Conta $conta, string $novoNome): void
+    public function alteraDados(): void
     {
-        if ($conta->getNomeUsuario() === $novoNome){
-            echo "O nome de usuário informado é igual ao já cadastrado." . PHP_EOL;
+        echo "[ 1 ] Alterar nome\n[ 2 ] Adicionar endereço\n[ 3 ] Atualizar endereço\n[ 0 ] Cancelar\nEscolha uma opção: ";
+        $escolha = rtrim(fgets(STDIN)); // Captura o input do terminal.
+        $this->tratarEscolha($escolha);
+    }
+
+    public function tratarEscolha($escolha)
+    {
+        if ($escolha === "1") {
+            echo "Novo nome de usuário: ";
+            $novoNome = rtrim(fgets(STDIN));
+            if ($this->nomeUsuario === $novoNome) {
+                echo "O nome de usuário informado é igual ao já cadastrado." . PHP_EOL;
+                $this->alteraDados();
+            }
+            $this->nomeUsuario = $novoNome;
         }
 
-        $conta->setNomeUsuario($novoNome);
+        else if ($escolha === "2") {
+            echo "Cidade: ";
+            $nomeCidade = rtrim(fgets(STDIN));
+            echo "Bairro: ";
+            $nomeBairro = rtrim(fgets(STDIN));
+            echo "Rua: ";
+            $nomeRua = rtrim(fgets(STDIN));
+            echo "Número: ";
+            $numeroCasa = rtrim(fgets(STDIN));
+            array_push($this->endereco, $nomeCidade, $nomeBairro, $nomeRua, $numeroCasa);
+
+            var_dump($this->endereco);
+        }
+
+        else if ($escolha === "0")
+        {
+            echo "Saindo do sistema...";
+            exit();
+        }
+        
+        else 
+        {
+            echo PHP_EOL . "Opção inválida, por favor escolha uma das opções disponíveis." . PHP_EOL;
+            $this->alteraDados();
+        }
     }
     //---------------------------------------------------------------------------------------------
 }
