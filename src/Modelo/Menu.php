362e369f75eee8abc38387199a4d575f1940f4be
ArthurClemente <arthurclementte@hotmail.com>
2021-11-10 16:29:11 -0300

    Implementado o código da função tentaLogin que permite que um perfil seja criado com base em uma conta cadastrada.

--- src/Modelo/Menu.php
+++ src/Modelo/Menu.php
@@ -5,6 +5,7 @@ namespace Projeto\Iface\Modelo;
 require_once "src/Modelo/Autoloader/autoload.php";
 
 use Projeto\Iface\Modelo\Conta\Conta;
+use Projeto\Iface\Modelo\Conta\Perfil;
 use Projeto\Iface\Modelo\Service\ListaRegistros;
 
 class Menu
@@ -12,26 +13,25 @@ class Menu
 
     public function __construct()
     {
-        $this->mostrarOpcoes();
+        $this->mostrarOpcoesMenu();
     }
 
-    public function mostrarOpcoes(): void
+    public function mostrarOpcoesMenu(): void
     {
         echo "[ 1 ] Login\n[ 2 ] Registro\n[ 0 ] Sair\nEscolha uma opção: ";
         $escolha = rtrim(fgets(STDIN));// Captura o input do terminal.
-        $this->tratarEscolha($escolha);
+        $this->tratarEscolhaMenu($escolha);
     }
 
-    public function tratarEscolha(string $escolha)
+    public function tratarEscolhaMenu(string $escolha)
     {
-        if($escolha === "1") // Falta implementar o método de tentar login
+        if($escolha === "1")
         {
             echo "Email: ";
             $emailLogin = rtrim(fgets(STDIN));
             echo "Senha: ";
             $senhaLogin = rtrim(fgets(STDIN));
             $this->tentaLogin($emailLogin, $senhaLogin);
-            $this->mostrarOpcoes();
         }
         
         else if($escolha === "2")
@@ -43,7 +43,7 @@ class Menu
             echo "Nome de usuário: ";
             $novoNomeDeUsuario = rtrim(fgets(STDIN));
             new Conta($novoEmail, $novaSenha, $novoNomeDeUsuario);
-            $this->mostrarOpcoes();
+            $this->mostrarOpcoesMenu();
         }
 
         else if ($escolha === "0")
@@ -55,7 +55,7 @@ class Menu
         else 
         {
             echo PHP_EOL . "Opção inválida, por favor escolha uma das três opções disponíveis." . PHP_EOL;
-            $this->mostrarOpcoes();
+            $this->mostrarOpcoesMenu();
         }
     }
 
@@ -67,10 +67,13 @@ class Menu
             for($dadosDaConta = 0; $dadosDaConta < 3; $dadosDaConta++) {
                 if($indiceConta[0] === $email && $indiceConta[1] === $senha)
                 {
-                    echo "Login bem sucedido!";
+                    echo PHP_EOL . "Login bem sucedido!" . PHP_EOL . PHP_EOL;
+                    $perfilLogado = new Perfil($email, $senha, $indiceConta[2]);
+                    $perfilLogado->opcoesPerfil();
                 }
             }
         }
-        echo "Dados de login não encontrados ou incorretos!" . PHP_EOL;
+        echo PHP_EOL . "Dados de login não encontrados ou incorretos!" . PHP_EOL;
+        $this->mostrarOpcoesMenu();
     }
 }
