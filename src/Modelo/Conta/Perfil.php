f6a5657f6759306193792bb274f9d75e3bd4293e
ArthurClemente <arthurclementte@hotmail.com>
2021-11-10 16:52:04 -0300

    Construtor alterado para possibilitar a criação do perfil dentro da classe Menu, implementada a opção de recuperar os dados do perfil e pequenas alterações nas exibições de mensagens foram feitas.

--- src/Modelo/Conta/Perfil.php
+++ src/Modelo/Conta/Perfil.php
@@ -7,7 +7,7 @@ require_once 'src/Modelo/Autoloader/autoload.php';
 use Projeto\Iface\Modelo\Comunidade;
 use Projeto\Iface\Modelo\Conta\Conta;
 
-class Perfil
+class Perfil extends Conta
 {
     private $email;
     private $senha;
@@ -15,31 +15,33 @@ class Perfil
     public $endereco = array();
     public $telefone;
 
-    public function __construct(Conta $conta)
+    public function __construct(string $email, string $senha, string $nomeUsuario)
     {
-        $this->email = $conta->getEmail();
-        $this->senha = $conta->getSenha();
-        $this->nomeUsuario = $conta->getNomeUsuario();
+        $this->email = $email;
+        $this->senha = $senha;
+        $this->nomeUsuario = $nomeUsuario;
     }
 
     //-------------------Funções de criação/alteração de atributos do perfil-----------------------
-    public function alteraDados(): void
+    public function opcoesPerfil(): void
     {
-        echo "[ 1 ] Alterar nome\n[ 2 ] Adicionar endereço\n[ 3 ] Atualizar endereço\n[ 4 ] Criar Comunidade\n[ 0 ] Cancelar\nEscolha uma opção: ";
+        echo "[ 1 ] Alterar nome\n[ 2 ] Adicionar endereço\n[ 3 ] Atualizar endereço\n[ 4 ] Criar Comunidade\n[ 5 ] Verificar dados do perfil\n[ 0 ] Cancelar\nEscolha uma opção: ";
         $escolhaPerfil = rtrim(fgets(STDIN)); // Captura o input do terminal.
-        $this->tratarEscolha($escolhaPerfil);
+        $this->tratarEscolhaPefil($escolhaPerfil);
     }
 
-    public function tratarEscolha($escolhaPerfil)
+    public function tratarEscolhaPefil($escolhaPerfil)
     {
         if ($escolhaPerfil === "1") {
             echo "Novo nome de usuário: ";
             $novoNome = rtrim(fgets(STDIN));
             if ($this->nomeUsuario === $novoNome) {
-                echo "O nome de usuário informado é igual ao já cadastrado." . PHP_EOL;
-                $this->alteraDados();
+                echo PHP_EOL . "O nome de usuário informado é igual ao já cadastrado." . PHP_EOL . PHP_EOL;
+                $this->opcoesPerfil();
             }
+            echo PHP_EOL . "Nome alterado de $this->nomeUsuario para $novoNome" . PHP_EOL . PHP_EOL;
             $this->nomeUsuario = $novoNome;
+            $this->opcoesPerfil();
         }
 
         else if ($escolhaPerfil === "2") {
@@ -52,6 +54,8 @@ class Perfil
             echo "Número: ";
             $numeroCasa = rtrim(fgets(STDIN));
             array_push($this->endereco, $nomeCidade, $nomeBairro, $nomeRua, $numeroCasa);
+            echo PHP_EOL . "Endereço cadastrado!" . PHP_EOL . PHP_EOL;
+            $this->opcoesPerfil();
         }
 
         else if ($escolhaPerfil === "4")
@@ -61,18 +65,26 @@ class Perfil
             echo "Descrição da Comunidade: ";
             $novaDescricao = rtrim(fgets(STDIN));
             new Comunidade($novoNomeComunidade, $novaDescricao);
+            echo PHP_EOL . "Comunidade $novoNomeComunidade criada" . PHP_EOL . PHP_EOL;
+            $this->opcoesPerfil();
+        }
+
+        else if ($escolhaPerfil === "5")
+        {
+            echo PHP_EOL . "Email: $this->email\nNome de usuário: $this->nomeUsuario" . PHP_EOL . PHP_EOL;
+            $this->opcoesPerfil();
         }
 
         else if ($escolhaPerfil === "0")
         {
-            echo "Saindo do sistema...";
+            echo "Saindo do sistema..." . PHP_EOL;
             exit();
         }
         
         else 
         {
-            echo PHP_EOL . "Opção inválida, por favor escolhaPerfil uma das opções disponíveis." . PHP_EOL;
-            $this->alteraDados();
+            echo PHP_EOL . "Opção inválida, por favor escolhaPerfil uma das opções disponíveis." . PHP_EOL . PHP_EOL;
+            $this->opcoesPerfil();
         }
     }
     //---------------------------------------------------------------------------------------------
