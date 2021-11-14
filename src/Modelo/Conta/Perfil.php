cf8cec9ae95cb231034ff07eacf9ad07a8495af0
ArthurClemente <arthurclementte@hotmail.com>
2021-11-14 16:32:39 -0300

    Implementada a opção de ver os dados da comunidade criada pelo perfil, de sair da conta e alterada a opção de criar comunidade.

--- src/Modelo/Conta/Perfil.php
+++ src/Modelo/Conta/Perfil.php
@@ -6,6 +6,8 @@ require_once 'src/Modelo/Autoloader/autoload.php';
 
 use Projeto\Iface\Modelo\Comunidade;
 use Projeto\Iface\Modelo\Conta\Conta;
+use Projeto\Iface\Modelo\Menu;
+use Projeto\Iface\Modelo\Service\ListaRegistros;
 
 class Perfil extends Conta
 {
@@ -14,19 +16,22 @@ class Perfil extends Conta
     private $nomeUsuario;
     public $endereco = array();
     public $telefone;
+    private $registrosComunidade;
 
     public function __construct(string $email, string $senha, string $nomeUsuario)
     {
         $this->email = $email;
         $this->senha = $senha;
         $this->nomeUsuario = $nomeUsuario;
+        $this->registrosComunidade = new ListaRegistros();
     }
 
     //-------------------Funções de criação/alteração de atributos do perfil-----------------------
     public function opcoesPerfil(): void
     {
-        echo "[ 1 ] Alterar nome\n[ 2 ] Adicionar endereço\n[ 3 ] Atualizar endereço\n[ 4 ] Criar Comunidade\n[ 5 ] Verificar dados do perfil\n[ 0 ] Cancelar\nEscolha uma opção: ";
+        echo "[ 1 ] Alterar nome\n[ 2 ] Adicionar endereço\n[ 3 ] Atualizar endereço\n[ 4 ] Criar Comunidade\n[ 5 ] Verificar dados do perfil\n[ 6 ] Verificar dados da comunidade\n[ 7 ] Fazer LogOff\n[ 0 ] Sair\nEscolha uma opção: ";
         $escolhaPerfil = rtrim(fgets(STDIN)); // Captura o input do terminal.
+
         $this->tratarEscolhaPefil($escolhaPerfil);
     }
 
@@ -35,6 +40,7 @@ class Perfil extends Conta
         if ($escolhaPerfil === "1") {
             echo "Novo nome de usuário: ";
             $novoNome = rtrim(fgets(STDIN));
+
             if ($this->nomeUsuario === $novoNome) {
                 echo PHP_EOL . "O nome de usuário informado é igual ao já cadastrado." . PHP_EOL . PHP_EOL;
                 $this->opcoesPerfil();
@@ -53,6 +59,7 @@ class Perfil extends Conta
             $nomeRua = rtrim(fgets(STDIN));
             echo "Número: ";
             $numeroCasa = rtrim(fgets(STDIN));
+
             array_push($this->endereco, $nomeCidade, $nomeBairro, $nomeRua, $numeroCasa);
             echo PHP_EOL . "Endereço cadastrado!" . PHP_EOL . PHP_EOL;
             $this->opcoesPerfil();
@@ -64,8 +71,12 @@ class Perfil extends Conta
             $novoNomeComunidade = rtrim(fgets(STDIN));
             echo "Descrição da Comunidade: ";
             $novaDescricao = rtrim(fgets(STDIN));
-            new Comunidade($novoNomeComunidade, $novaDescricao);
-            echo PHP_EOL . "Comunidade $novoNomeComunidade criada" . PHP_EOL . PHP_EOL;
+
+            $nomeCriador = $this->nomeUsuario;
+            $novaComunidade = new Comunidade($nomeCriador, $novoNomeComunidade, $novaDescricao);
+            $this->registrosComunidade->incrementaArrayComunidades($novaComunidade);
+
+            echo PHP_EOL . "Comunidade ($novoNomeComunidade) criada" . PHP_EOL . PHP_EOL;
             $this->opcoesPerfil();
         }
 
@@ -75,9 +86,36 @@ class Perfil extends Conta
             $this->opcoesPerfil();
         }
 
+        else if ($escolhaPerfil === '6')
+        {
+            $listaComunidades = $this->registrosComunidade->getRegistroComunidades();
+            if(empty($listaComunidades))
+            {
+                echo PHP_EOL . "Você não possui nenhuma comunidade!" . PHP_EOL . PHP_EOL;
+                $this->opcoesPerfil();
+            }
+            foreach($listaComunidades as $comunidades)
+            {
+                if($comunidades->getNomeCriador() === $this->nomeUsuario)
+                {
+                    $nomeComunidade = $comunidades->getNomeComunidade();
+                    $descricaoComunidade = $comunidades->getDescricaoComunidade();
+                    $membrosComunidade = $comunidades->getMembrosComunidade();
+                    echo PHP_EOL . "Nome da comunidade: $nomeComunidade\nDescrição da comunidade: $descricaoComunidade\nMembros: $membrosComunidade" . PHP_EOL . PHP_EOL;
+                }
+            }
+            $this->opcoesPerfil();
+        }
+
+        else if ($escolhaPerfil === "7")
+        {
+            echo PHP_EOL . "Saindo da conta..." . PHP_EOL . PHP_EOL;
+            new Menu();
+        }
+
         else if ($escolhaPerfil === "0")
         {
-            echo "Saindo do sistema..." . PHP_EOL;
+            echo PHP_EOL . "Saindo do sistema..." . PHP_EOL;
             exit();
         }
         
