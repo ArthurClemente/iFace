0cf4de91b092a1f3b12e5fa7b5ba959a9e4be68b
ArthurClemente <arthurclementte@hotmail.com>
2021-11-15 17:37:28 -0300

    Registro de comunidades realocado para a classe Perfil.

--- src/Modelo/Service/ListaRegistros.php
+++ src/Modelo/Service/ListaRegistros.php
@@ -7,7 +7,7 @@ use Projeto\Iface\Modelo\Comunidade;
 class ListaRegistros
 {
     private static $registroContas = array();
-    private static $registroComunidades = array();
+
     
     public function incrementaArrayContas(string $email, string $senha, string $nomeUsuario): void
     {
@@ -15,18 +15,8 @@ class ListaRegistros
         array_push(self::$registroContas, $dadosConta);
     }
 
-    public function incrementaArrayComunidades(Comunidade $novaComunidade): void
-    {
-        array_push(self::$registroComunidades, $novaComunidade);
-    }
-
     public function getRegistroContas(): array
     {
         return self::$registroContas;
-    }
-
-    public function getRegistroComunidades(): array
-    {
-        return self::$registroComunidades;
     }
 }
